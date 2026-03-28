43441293b6a0c1ce3f2443e2648061020363ebd3
Oscar Martínez <oaum12@gmail.com>
2026-03-27 23:28:05 -0600

    agregar faltante de guia 1

--- src/services/usersServices.js
+++ src/services/usersServices.js
@@ -34,3 +34,68 @@ export const getBuscarNombre = async (nombre) => {
 
   return result.rows;
 };
+
+export const postCrearUsuario = async (
+  nombre,
+  documento,
+  carnet,
+  email,
+  contrasenia,
+) => {
+  try {
+    const query = `INSERT INTO doc.usuarios (nombre, documento, carnet, email, contrasenia, bloqueado, ultimo_login, activo) 
+    VALUES ($1, $2, $3, $4, $5, 'N', null,'A') RETURNING *;`;
+
+    const result = await pool.query(query, [
+      nombre,
+      documento,
+      carnet,
+      email,
+      contrasenia,
+    ]);
+
+    return result.rows[0];
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const actualizarUsuario = async (usuario) => {
+  const query = `UPDATE doc.usuarios SET nombre=$1, documento=$2, carnet=$3, email=$4 , contrasenia=$5 
+  WHERE id_usuario = $6 RETURNING *;`;
+
+  try {
+    const result = await pool.query(query, usuario);
+
+    if (result.rowCount === 0)
+      throw new Error("Usuario no encontrado");
+
+    return result.rows[0];
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const eliminarUsuario = async (id_usuario) => {
+  try {
+    const usuarioEliminar = await pool.query(
+      "SELECT * FROM doc.usuarios WHERE id_usuario=$1",
+      [id_usuario],
+    );
+
+    if (usuarioEliminar.rowCount === 0)
+      throw new Error("Usuario no encontrado");
+
+    const result = await pool.query(
+      "DELETE FROM doc.usuarios WHERE id_usuario=$1",
+      [id_usuario],
+    );
+
+    return {
+      message: "Usuario eliminado correctamente",
+      usuario: usuarioEliminar.rows[0],
+    };
+  } catch (error) {
+    return { error: error.message };
+  }
+};
